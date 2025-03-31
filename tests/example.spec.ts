b932f39120cb824001955ff72d31ddf5f07bca7a
angiecornejo <angeles.cornejo.ruata@gmail.com>
2025-03-31 17:12:47 -0300

    Make resuable tests using beforeEach for 'context actions'

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('stock_validation', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('https://www.amazon.com/');
   // Click different Image button to skip the "robot check" (sometimes it shows and sometimes it doesnt)
   const skip = await page.locator('//a[text()="Try different image"]')
@@ -11,7 +11,9 @@ test('stock_validation', async ({ page }) => {
   // Select the option for the webSite to be in English (sometimes it automatically translates to spanish)
   await page.locator('//div[@id="icp-nav-flyout"]/button').click();
   await page.locator('//div[@id="nav-flyout-icp"]//a[@lang="en-US"]').click();
-  
+});
+
+test('stock_validation', async ({ page }) => {
   await page.locator('input[id="twotabsearchtextbox"]').fill('wireless headphones');
   await page.keyboard.press('Enter');
   
@@ -22,4 +24,8 @@ test('stock_validation', async ({ page }) => {
   await page.locator('//div[@role="listitem"][4]//h2').dblclick();
   
   await expect(page.locator('//div[@id="availability"]')).toContainText('In Stock');	
+});
+
+test('cart_validation', async ({ page }) => {
+  
 });
